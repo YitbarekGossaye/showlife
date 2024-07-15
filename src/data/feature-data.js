722fc9f9935c8b8942fab38af4bd977e6015516a
Yitbarek <gossayeyitbarek@gmail.com>
2024-07-15 07:38:01 +0300

    first commit

---
+++ src/data/feature-data.js
@@ -0,0 +1,45 @@
+
+import  feature_img_1 from "../../public/assets/img/feature/web_dev.png"
+import  feature_img_2 from "../../public/assets/img/feature/web_host.png"
+import  feature_img_3 from "../../public/assets/img/feature/digital_market.png"
+import  feature_img_4 from "../../public/assets/img/feature/coffee.png"
+
+const feature_data = [
+    {
+        id: 1, 
+        color: "1",
+        img: feature_img_1,
+        title: <>Web Development</>,
+        description: <>Transfer contacts information <br />
+        between MailChimp</>,
+
+    },
+    {
+        id: 2, 
+        color: "2",
+        img: feature_img_2,
+        title: <>Web Hosting</>,
+        description: <>Transfer contacts information <br />
+        between MailChimp</>,
+
+    },
+    {
+        id: 3, 
+        color: "3",
+        img: feature_img_3,
+        title: <>Digital Marketing</>,
+        description: <>Transfer contacts information <br />
+        between MailChimp</>,
+
+    },
+    {
+        id: 4, 
+        color: "4",
+        img: feature_img_4,
+        title: <>Ye-Buna</>,
+        description: <>Transfer contacts information <br />
+        between MailChimp</>,
+
+    },
+]
+export default feature_data
